12e77b40e6d84409603f8c219f8d862ea40e432e
Hakkinen Wylgner <HakkinenWylgner@gmail.com>
2023-02-26 22:33:52 -0300

    Conclusão ensino básico

--- app/src/main/java/com/example/notificaotoast/MainActivity.java
+++ app/src/main/java/com/example/notificaotoast/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
     public void abrirToast(View view){
-        Toast.makeText(
+        //Forma com que de mais liberdade para criar notificação
+        ImageView imagem = new ImageView (getApplicationContext());
+        imagem.setImageResource(android.R.drawable.star_big_off);
+
+        TextView textView = new TextView(getApplicationContext());
+        textView.setBackgroundResource(R.color.purple_200);
+        textView.setText("Olá Toast");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(toast.LENGTH_LONG);
+        //toast.setView( imagem );
+        toast.setView(textView);
+        toast.show();
+
+        //Forma fácil de notificação
+/*        Toast.makeText(
                 getApplicationContext(),
                 "Ação realizada com sucesso!",
                 Toast.LENGTH_SHORT
-        ).show();
+        ).show();*/
     }
 
 
